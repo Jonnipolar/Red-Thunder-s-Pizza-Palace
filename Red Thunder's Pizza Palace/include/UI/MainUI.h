caf03b995300ac7c67b03d067231dca7db265017
Kjartan Már Andersen <svidakjammi@gmail.com>
2017-12-04 15:57:23 +0000

    Update MainUI.h

--- Red Thunder's Pizza Palace/include/UI/MainUI.h
+++ Red Thunder's Pizza Palace/include/UI/MainUI.h
@@ -13,10 +13,8 @@ class MainUI
         MainUI();
         /// only function in class, sets up UI when logging in
         void startUI();
-<<<<<<< HEAD:Red Thunder's Pizza Palace/include/UI/MainUI.h
         /// UI for Manager
-=======
->>>>>>> b6c3ff64e503bb22198904bd633c8458244c5475:Red Thunder's Pizza Palace/MainUI.h
+        
     private:
         /// to type in topping
         void make_topping();
