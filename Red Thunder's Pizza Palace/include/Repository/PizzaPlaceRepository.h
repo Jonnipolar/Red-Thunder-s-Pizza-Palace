ef8034e68bc53982ab64fb4476464f4d7192f576
JonniPolar <jonj16@ru.is>
2017-12-08 12:02:25 +0000

    gerdi skrifa i vector i pizzplacerepository

--- Red Thunder's Pizza Palace/include/Repository/PizzaPlaceRepository.h
+++ Red Thunder's Pizza Palace/include/Repository/PizzaPlaceRepository.h
@@ -2,6 +2,9 @@
 #define PIZZAPLACEREPOSITORY_H
 #include "PizzaPlace.h"
 #include <fstream>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
 using namespace std;
 
 
@@ -11,6 +14,7 @@ class PizzaPlaceRepository
         PizzaPlaceRepository();
         virtual ~PizzaPlaceRepository();
         void store_pizza_places(const PizzaPlace& pizza_place);
+        vector <PizzaPlace> get_pizza_place();
     private:
 };
 
--- Red Thunder's Pizza Palace/src/Repository/PizzaPlaceRepository.cpp
+++ Red Thunder's Pizza Palace/src/Repository/PizzaPlaceRepository.cpp
@@ -17,3 +17,21 @@ void PizzaPlaceRepository::store_pizza_places(const PizzaPlace& pizza_place) {
         fout.close();
     }
 }
+vector <PizzaPlace> PizzaPlaceRepository::get_pizza_place() {
+    vector <PizzaPlace> pizza_place;
+    ifstream fin("PizzaPlaces.txt");
+    if(fin.is_open()) {
+        string str;
+        while (getline(fin, str)) {
+            int index = str.find(':');
+            int index_second = str.find(':', index +1);
+            string street = str.substr(0, index);
+            string number = str.substr(index + 1, (index_second - 1) - index);
+            int _number = atoi(number.c_str());
+            PizzaPlace pizza_places(street, _number);
+            pizza_place.push_back(pizza_places);
+        }
+        fin.close();
+    }
+    return pizza_place;
+}
