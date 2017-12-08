e71e53fd9cf33872ed3d8efcb4940d53d230d25b
JonniPolar <jonj16@ru.is>
2017-12-08 12:25:04 +0000

    gerdi skrifa i vector i pizzbottomrepository

--- Red Thunder's Pizza Palace/include/Repository/PizzaBottomRepository.h
+++ Red Thunder's Pizza Palace/include/Repository/PizzaBottomRepository.h
@@ -2,13 +2,17 @@
 #define PIZZABOTTOMREPOSITORY_H
 #include <fstream>
 #include "PizzaBottom.h"
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
 
 using namespace std;
 
 class PizzaBottomRepository
 {
     public:
-        void store_pizza_bottom(const PizzaBottom pizza_bottom);
+        void store_pizza_bottom(const PizzaBottom& pizza_bottom);
+        vector <PizzaBottom> get_pizza_bottom();
     private:
 };
 
--- Red Thunder's Pizza Palace/src/Repository/PizzaBottomRepository.cpp
+++ Red Thunder's Pizza Palace/src/Repository/PizzaBottomRepository.cpp
@@ -1,10 +1,28 @@
 #include "PizzaBottomRepository.h"
 
-void PizzaBottomRepository::store_pizza_bottom(const PizzaBottom pizza_bottom) {
+void PizzaBottomRepository::store_pizza_bottom(const PizzaBottom& pizza_bottom) {
     ofstream fout;
     fout.open("PizzaBottom.txt", ios::app);
     if(fout.is_open()){
         fout << pizza_bottom;                        /// prints to .txt and is not final, just made it to test
         fout.close();
     }
+}
+vector <PizzaBottom> PizzaBottomRepository::get_pizza_bottom() {
+    vector <PizzaBottom> pizza_bottoms;
+    ifstream fin("PizzaBottom.txt");
+    if (fin.is_open()) {
+        string str;
+        while (getline(fin, str)) {
+            int index = str.find(':');
+            int index_second = str.find(':', index + 1);
+            string type = str.substr(0, index);
+            string price = str.substr(index +1, (index_second - 1) - index);
+            int _price = atoi(price.c_str());
+            PizzaBottom pizza_bottom(type, _price);
+            pizza_bottoms.push_back(pizza_bottom);
+        }
+        fin.close();
+    }
+    return pizza_bottoms;
 }
