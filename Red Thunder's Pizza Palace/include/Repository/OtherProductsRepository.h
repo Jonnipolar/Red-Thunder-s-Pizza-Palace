2c12907a2505c09e1a0ca529e4d06db920862289
JonniPolar <jonj16@ru.is>
2017-12-09 17:46:05 +0000

    bjo til nyjan tomann repo class for otherproducts

---
+++ Red Thunder's Pizza Palace/include/Repository/OtherProductsRepository.h
@@ -0,0 +1,16 @@
+#ifndef OTHERPRODUCTSREPOSITORY_H
+#define OTHERPRODUCTSREPOSITORY_H
+
+
+class OtherProductsRepository
+{
+    public:
+        OtherProductsRepository();
+        virtual ~OtherProductsRepository();
+
+    protected:
+
+    private:
+};
+
+#endif // OTHERPRODUCTSREPOSITORY_H
---
+++ Red Thunder's Pizza Palace/src/Repository/OtherProductsRepository.cpp
@@ -0,0 +1,11 @@
+#include "OtherProductsRepository.h"
+
+OtherProductsRepository::OtherProductsRepository()
+{
+    //ctor
+}
+
+OtherProductsRepository::~OtherProductsRepository()
+{
+    //dtor
+}
