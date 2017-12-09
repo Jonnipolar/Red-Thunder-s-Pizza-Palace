24031282ed6d8466c23f3e3ebd0ff891fb0a9c50
Kjartan Már Andersen <svidakjammi@gmail.com>
2017-12-09 14:35:00 +0000

    Add time to Order Class

--- Red Thunder's Pizza Palace/include/Models/Order.h
+++ Red Thunder's Pizza Palace/include/Models/Order.h
@@ -3,20 +3,24 @@
 #include <iostream>
 #include <vector>
 #include "Pizza.h"
+#include <ctime>
 
 using namespace std;
 class Order
 {
     public:
         Order();
+        Order(string name, vector <Pizza> pizzas, string OrderTime);
         string get_name();
         vector <Pizza> get_pizzas();
+        string get_time();
+        void set_time();
         ~Order();
 
     private:
         string name;
         vector <Pizza> pizzas;
-
+        string OrderTime;
 };
 
 #endif // ORDER_H
--- Red Thunder's Pizza Palace/src/Models/Order.cpp
+++ Red Thunder's Pizza Palace/src/Models/Order.cpp
@@ -2,13 +2,33 @@
 
 Order::Order()
 {
-    //ctor
+    this->name = "No Order";
+    Pizza pizza;
+    this->pizzas.push_back(pizza);
+    set_time();
+
+}
+Order::Order(string name, vector <Pizza> pizzas, string OrderTime) {
+    this->name = name;
+    this->pizzas = pizzas;
+    this->OrderTime = OrderTime;
 }
 string Order::get_name() {
     return this->name;
 }
 vector <Pizza> Order::get_pizzas() {
     return this->pizzas;
+}
+string Order::get_time() {
+    return this->OrderTime;
+}
+void Order::set_time() {
+    time_t timestamp = time(0);
+    char time_buffer[80];
+    strftime(time_buffer, sizeof(time_buffer), "%d/%m/%Y %H:%M ", localtime(&timestamp));
+
+    string TimeOrder(time_buffer);
+    this->OrderTime = TimeOrder;
 }
 Order::~Order()
 {
