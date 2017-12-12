013fbb833474ff76a5c96a6f7de5519deac02101
Kjartan Már Andersen <svidakjammi@gmail.com>
2017-12-12 11:41:02 +0000

    Add Bakery

---
+++ Red Thunder's Pizza Palace/include/UI/BakeryUI.h
@@ -0,0 +1,15 @@
+#ifndef BAKERYUI_H
+#define BAKERYUI_H
+
+
+class BakeryUI
+{
+    public:
+        BakeryUI();
+        ~BakeryUI();
+        void StartUI();
+
+    private:
+};
+
+#endif // BAKERYUI_H
---
+++ Red Thunder's Pizza Palace/src/UI/BakeryUI.cpp
@@ -0,0 +1,13 @@
+#include "BakeryUI.h"
+
+BakeryUI::BakeryUI()
+{
+    //ctor
+}
+void BakeryUI::StartUI() {
+
+}
+BakeryUI::~BakeryUI()
+{
+    //dtor
+}
