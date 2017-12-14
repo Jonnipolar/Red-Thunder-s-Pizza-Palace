e64a83b7d8a6e6668349aec4402c7842ace13d18
JonniPolar <jonj16@ru.is>
2017-12-14 11:43:03 +0000

    Bjo til dilivery klasann

---
+++ Red Thunder's Pizza Palace/include/UI/DeliveryUI.h
@@ -0,0 +1,16 @@
+#ifndef DELIVERYUI_H
+#define DELIVERYUI_H
+
+
+class DeliveryUI
+{
+    public:
+        DeliveryUI();
+        virtual ~DeliveryUI();
+
+    protected:
+
+    private:
+};
+
+#endif // DELIVERYUI_H
